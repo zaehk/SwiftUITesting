0e4d993135af632c83eed26a0440396c8599581b
Borja Sáez de Guinoa Vilaplana <borja.saez.de.guinoa.vilaplana@everis.com>
2019-06-21 14:39:01 +0200

    initial commit

---
+++ SwiftUITesting/SwiftUITesting/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITesting
+//
+//  Created by Borja Saez de Guinoa Vilaplana on 21/06/2019.
+//  Copyright © 2019 Borja Saez de Guinoa Vilaplana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
